af9b990acb445ab3adc558930e9062466e3b3b56
Mónica Polanco <mpolan16@g.holycross.edu>
2020-09-09 22:57:41 -0400

    added regEx solution

---
+++ SplitandIncludesPractice.js
@@ -0,0 +1,17 @@
+// Broken Bridge
+// Create a function which validates whether a bridge is safe to walk on (i.e. has no gaps in it to fall through).
+
+// Examples
+// isSafeBridge("####") ➞ true
+
+// isSafeBridge("## ####") ➞ false
+
+// isSafeBridge("#") ➞ true
+// Notes
+// You can expect the bridge's ends connecting it to its surrounding.
+
+function isSafeBridge(str) {
+	let sepChar = str.split('');
+	
+		return !sepChar.includes(' ');
+}
