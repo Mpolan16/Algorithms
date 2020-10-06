11f2b92b168fe3aca12da799b17175f6c96961a6
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-05 21:18:45 -0400

    added solutions to dotfliter file

---
+++ dotfilter.js
@@ -0,0 +1,16 @@
+// Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
+
+// Examples
+
+// charCount("a", "edabit") ➞ 1
+
+// charCount("c", "Chamber of secrets") ➞ 1
+
+// charCount("b", "big fat bubble") ➞ 4
+// Notes
+
+// Your output must be case-sensitive (see second example).
+
+function charCount(myChar, str) {
+	return (str.split("").filter(char => char === myChar)).length;
+}
