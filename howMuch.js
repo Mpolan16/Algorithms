3b0fdb84c7dd9467f81c0f1d5700552cbf602e98
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-20 13:28:59 -0400

    added solution

---
+++ howMuch.js
@@ -0,0 +1,27 @@
+// Create a function which returns the number of true values there are in an array.
+
+// Examples
+
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+// Notes
+
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
+
+function countTrue(arr) {
+    if (arr.length === 0) {
+        return 0;
+    } else {
+    let count = 0;
+    for (let b in arr){
+        if(arr[b] === true){
+            count++;
+        }
+    }
+    return count;
+    }
+}
