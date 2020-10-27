04b3bdf8d4e4743ea2b19694c6a3c114698341ff
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-26 21:09:13 -0400

    added solution

---
+++ slidingWindow.js
@@ -0,0 +1,20 @@
+// this pattern involves creating a window which can either be an array or number from one position to another. Depending on a certian condition, the window either increases or closes (and a new window is created). Very useful for keeing track of subset of data in an array/string etc.
+
+//not sorted elements
+
+//Write a function called maxSubarraySum which accepts an array of integers and number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum +=arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
