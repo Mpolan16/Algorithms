9e454a4198e8ab369d51408205c9fdc30c1cc606
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-27 10:53:31 -0400

    solution

--- recursive.js
+++ recursive.js
@@ -1,21 +1,23 @@
-// function countDown(num){
-//     if(num <= 0) {
-//         console.log("All done!");
-//         return;
-//     }
-//     console.log(num);
-//     num--;
-//     countDown(num);
-// }
+function countDown(num){
+    if(num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
 
 //recursive function;
 //1. base case- when recusrion stops
 //2. Different peice of data
 
-function countDown(num){
-    for(var i = num; i > 0; i--){
-        console.log(i);
-    }
-    console.log("All done!");
-}
-countDown(5);
+//how we would do it with a loop not recursively
+// function countDown(num){
+//     for(var i = num; i > 0; i--){
+//         console.log(i);
+//     }
+//     console.log("All done!");
+// }
+// countDown(5);
+
