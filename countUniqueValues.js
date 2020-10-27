a120f7b54e0240e331af3a73df8ba32260813b14
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-26 20:16:42 -0400

    added new count unique value problem

---
+++ countUniqueValues.js
@@ -0,0 +1,4 @@
+//this function accepts a sorted array, and counts the unique values in the array, there can be negative numbers in the array, but it will always be sorted.
+
+//basically tells you how many of each kind of number there are so [1,2,2,2] has 2 unique numbers and it will return 2.
+
