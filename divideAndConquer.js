8a81970e125e4258fb01c004e303e42c7c7abec5
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-26 21:32:23 -0400

    added more to the solution

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -6,18 +6,20 @@
 
 //Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the vlaue is not found, return -1.
 
+//sorted arrays only**
+
 function search(array, val) {
     let min = 0;
     let max = array.length -1;
 
     while (min <= max){
-        let middle = Math.floor((min +max) / 2);
+        let middle = Math.floor((min + max) / 2);
         let currentElement = array[middle];
 
-        if (array[middle] < val) {
+        if (currentElement < val) {
             min = middle + 1;
         }
-        else if( array[middle] > val){
+        else if( currentElement > val){
             max = middle -1;
         }  
         else {
