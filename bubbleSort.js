a9b030c5bdd68f7a136a707eca368257d579ebb0
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-28 14:26:17 -0400

    added more info on solution

--- bubbleSort.js
+++ bubbleSort.js
@@ -30,3 +30,24 @@ function bubbleSort(arr){
   }
   
   bubbleSort([8,1,2,3,4,5,6,7]);
+
+  // Optimized BubbleSort with noSwaps
+  //*HERE the no swap is what is allowing it to not continue on over and over after it is finally sorted as is the default of the bubble method. it will continue through until the noSwaps var is true */
+function bubbleSort(arr){
+    var noSwaps;
+    for(var i = arr.length; i > 0; i--){
+      noSwaps = true;
+      for(var j = 0; j < i - 1; j++){
+        if(arr[j] > arr[j+1]){
+          var temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          noSwaps = false;         
+        }
+      }
+      if(noSwaps) break;
+    }
+    return arr;
+  }
+  
+  bubbleSort([8,1,2,3,4,5,6,7]);
