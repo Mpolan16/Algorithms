f4f8eb5dbbdacab882d762a1127f2bc24423cdac
Mónica Polanco <mpolan16@g.holycross.edu>
2020-10-31 19:18:26 -0400

    added a possible solution

--- maxFirstNum.js
+++ maxFirstNum.js
@@ -16,3 +16,16 @@
 
 // Each digit in the second number can only be used once.
 // Zero to all digits in the second number may be used.
+
+function maxPossible(n1, n2) {
+	var a1 = (""+n1).split('');
+	var a2 = (""+n2).split('').sort(function(a,b){
+		return b-a;
+	});
+	for (var i in a1) {
+		if (a1[i] < a2[0]) {
+			a1[i] = a2.shift();
+		}
+	}
+	return a1.join('') | 0;
+}
