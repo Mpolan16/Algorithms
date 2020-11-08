796929f58877275a5e7c0c745118891d88f092a5
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-08 17:55:15 -0500

    added a possible solution

--- diceGame.js
+++ diceGame.js
@@ -35,3 +35,12 @@ function add(a,b) {
     });
       return results.includes(0) ? 0 : results.reduce(add);
     }
+
+    //another possible solution
+
+    function diceGame(arr) {
+		for(let i of arr){
+		if(i[0] == i[1]) return 0;
+	}
+		 return arr.flat().reduce((c,i) => c + i, 0);
+}
