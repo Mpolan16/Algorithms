b752bb2755dfd5b028a4283a553cee537908ac89
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-10 15:15:45 -0500

    added a new prob set

---
+++ alternatingCharacters.js
@@ -0,0 +1,8 @@
+// Function Description
+// Complete the alternatingCharacters function in the editor below. It must return an integer representing the minimum number of deletions to make the alternating string.
+// alternatingCharacters has the following parameter(s):
+// s: a string
+
+//you have a string that only contains characters A and B. They reapeat and my job is to make A and B alternate by deleting the consecutive ones. The objective though is to achieve the alternation with deleting the smallest amount of charaters possible.
+
+
