962b30923803c5e956160aa53a3c5cbfe635bc1c
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-10 15:24:53 -0500

    added info for the algo

--- alternatingCharacters.js
+++ alternatingCharacters.js
@@ -5,4 +5,33 @@
 
 //you have a string that only contains characters A and B. They reapeat and my job is to make A and B alternate by deleting the consecutive ones. The objective though is to achieve the alternation with deleting the smallest amount of charaters possible.
 
+// q represetns the about of queries = how many strings they are giving me to fix
+
+//s represents the string and ONLY has A and B charaters
+
+//Output Format
+//for each query, prin the minumum nuber of seletions required on a new line
+
+//sample input
+
+// 5 --->q = query
+// AAAA ---> 1 string from the query etc...
+// BBBBB
+// ABABABAB
+// BABABA
+// AAABBB
+
+//Sample Output
+
+
+// 3 -- minimum i can delete here is 3 etc...
+// 4
+// 0
+// 0
+// 4
+
+// Explanation
+// The characters marked red are the ones that can be deleted so that the string doesn't have matching consecutive characters.
+
+
 
