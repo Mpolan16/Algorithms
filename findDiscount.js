6213018cffa159c0ee9502f330b0f76f8c2a6239
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-16 16:44:35 -0500

    added approach to prob set

---
+++ findDiscount.js
@@ -0,0 +1,19 @@
+// Create a function that takes two arguments: the final price and the discount percentage as integers and returns the final price after the discount.
+
+// Alternative Text
+
+// Examples
+
+// dis(1500, 50) ➞ 750
+
+// dis(89, 20) ➞ 71.2
+
+// dis(100, 75) ➞ 25
+// Notes
+
+// Your answer should be rounded to two decimal places.
+
+//APPROACH
+
+//Looks like a simple math problem
+//im planning to take the first arg "price" and then multiply it by "discount"/100 and return price - discount
