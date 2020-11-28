94d022536e4f29f393c1a9c4940485cf5f28e086
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-28 17:27:30 -0500

    added a resource

--- brickFits.js
+++ brickFits.js
@@ -12,3 +12,12 @@
 // You can turn the brick with any side towards the hole.
 // We assume that the brick fits if its sizes equal the ones of the hole (i.e. brick size should be less than or equal to the size of the hole, not strictly less).
 // You can't put a brick in at a non-orthogonal angle.
+
+// Resouce: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+// const array = [1, 2, 3, 4, 5];
+
+// // checks whether an element is even
+// const even = (element) => element % 2 === 0;
+
+// console.log(array.some(even));
+// // expected output: true
