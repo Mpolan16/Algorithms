48dca1441acd7add747e63f5ebfbe308b2be4c17
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-30 14:13:57 -0500

    added unshift answer

--- Data_Structures_Folder/unshift.js
+++ Data_Structures_Folder/unshift.js
@@ -1,7 +1,22 @@
 //thisfunction should accept a value
 //creat a new node using the value passed to the function
-// if there is no head property on the ListeningStateChangedEvent, set the head and tail to be the newly created Node
+// if there is no head property on the List, set the head and tail to be the newly created Node
 //otherwise set the newly created node's next property to be the current head property on the list
 //set the head property on th elist th ebe that newly created node
 //increment the length of th elist by 1
 //return the linked list
+
+unshift(val){
+    var newNode = new Node(val);
+
+    if(!this.head){
+        this.head = newNode;
+        this.tail = this.head;
+    } else{
+        newNode.next = this.head;
+        this.head = newNode;
+
+        this.length++;
+        return this;
+    }
+}
