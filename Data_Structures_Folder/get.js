294bbc60169f532e3467de440175702bec1f0084
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-30 14:21:45 -0500

    added pseudocode for get.js

---
+++ Data_Structures_Folder/get.js
@@ -0,0 +1,3 @@
+//this function should accept an index
+//if the index is less than zero or greater than or equal to the length of the list, return null
+//loop through the list until you reach the index and return the node at the specidic index--counter
