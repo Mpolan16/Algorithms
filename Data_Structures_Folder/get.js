cb70e4f993a693184be56884bae57ea01b47843b
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-30 14:30:03 -0500

    added solution to get.js

--- Data_Structures_Folder/get.js
+++ Data_Structures_Folder/get.js
@@ -1,3 +1,15 @@
 //this function should accept an index
 //if the index is less than zero or greater than or equal to the length of the list, return null
 //loop through the list until you reach the index and return the node at the specidic index--counter
+
+get(index){
+    if(index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+
+    while(counter !== index){
+        current = current.next;
+        counter++;
+    }
+    return current;
+}
