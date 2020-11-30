57df05b74fad2820cf329bb26a58af9b720ad18e
Mónica Polanco <mpolan16@g.holycross.edu>
2020-11-30 13:31:22 -0500

    added unshifting pseudocode for ds work

---
+++ Data_Structures_Folder/unshift.js
@@ -0,0 +1,7 @@
+//thisfunction should accept a value
+//creat a new node using the value passed to the function
+// if there is no head property on the ListeningStateChangedEvent, set the head and tail to be the newly created Node
+//otherwise set the newly created node's next property to be the current head property on the list
+//set the head property on th elist th ebe that newly created node
+//increment the length of th elist by 1
+//return the linked list
