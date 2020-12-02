0a529d007b4d3fba3641d931a48e6608793ec62e
Mónica Polanco <mpolan16@g.holycross.edu>
2020-12-02 15:44:40 -0500

    added solution

--- Algorithms_Folder/comparePoints.js
+++ Algorithms_Folder/comparePoints.js
@@ -50,3 +50,14 @@
 // Comparing the  and  elements,  and  so Alice receives two points. 
 // The return array is .
 
+function compareTriplets(a, b) {
+    let points = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            points[0]++;
+        } else if (a[i] != b[i]) {
+            points[1]++;
+        }
+    }
+    return points;
+}
