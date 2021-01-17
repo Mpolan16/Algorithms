6b6dc6d4832130e329a9c502d8e8f35860b3a588
Mónica Polanco <mpolan16@g.holycross.edu>
2021-01-17 12:58:09 -0500

    added skeleton for doubly ll

---
+++ Data_Structures_Folder/doubly_linked_lists/push.js
@@ -0,0 +1,19 @@
+// Node
+// -Val, next, prev
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+// DoublyLinkedList 
+// -Head,tail,length
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
